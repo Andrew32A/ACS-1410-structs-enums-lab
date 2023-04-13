56726f9d669a27429ea73eaa3e16baa0900bace3
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-12 20:22:23 -0700

    finished all challenges

--- Contents.swift
+++ Contents.swift
@@ -29,13 +29,13 @@ struct Order {
   let name: String
   
   func printDescription() {
-    print("\(name) ordered \(boba.tea) \(boba.hasBoba ? "boba" : "") tea, \(boba.sweetness)% sweetness, \(boba.hasBoba ? "with boba" : "no boba")")
+    print("\(name) ordered \(boba.tea) \(boba.hasBoba ? "boba" : "")tea, \(boba.sweetness)% sweetness, \(boba.hasBoba ? "with boba" : "no boba")")
   }
 }
 
 let boba = BobaTea(tea: "black", sweetness: 25, hasBoba: true)
-print(boba.tea) // black
-print(boba.sweetness) // 25
+//print(boba.tea) // black
+//print(boba.sweetness) // 25
 
 // functions
 func createBoba(teaType: String, sweetness: Int, hasBoba: Bool) -> BobaTea {
@@ -55,11 +55,11 @@ func createOrder(withTea tea: String, sweetness: Int, forCustomer customer: Stri
 }
 
 // instantiation and output
-let customer = Customer(firstName: "Monica", lastName: "Geller")
-print(customer.firstName) // Monica
-
-let newOrder = createOrder(withTea: "black", sweetness: 25, forCustomer: customer.firstName, includeBoba: true)
-newOrder.printDescription()
+//let customer = Customer(firstName: "Monica", lastName: "Geller")
+//print(customer.firstName) // Monica
+//
+//let newOrder = createOrder(withTea: "black", sweetness: 25, forCustomer: customer.firstName, includeBoba: true)
+//newOrder.printDescription()
 
 
 // enums
@@ -114,3 +114,58 @@ case .almond:
 case .oat:
     print("oat milk")
 }
+
+
+enum OrderFullfilment {
+  case success(message: String)
+  case error(message: String)
+}
+
+func makeOrder(order: Order) -> OrderFullfilment {
+    let date = Date()
+    let calendar = Calendar.current
+    let hour = calendar.component(.hour, from: date)
+    print(hour)
+    if hour < 17 && hour > 9{
+        return .success(message: "You can pick up your order in 30 min")
+    }else{
+        return .error(message: "We are closed, try tomorrow")
+    }
+}
+
+// my order
+let newcustomer = Customer(firstName: "Andrew", lastName: "Alsing")
+
+let myOrder = createOrder(withTea: "chai", sweetness: 50, forCustomer: "Andrew", includeBoba: false)
+myOrder.printDescription()
+
+let orderResult = makeOrder(order: myOrder)
+
+switch orderResult {
+case .success(let message):
+  print("Order result: \(message)")
+case .error(let message):
+  print("Order result: \(message)")
+}
+
+
+//Q1: Which of the following would be best represented with an enumeration?
+//
+//Names of people in a room
+//Political parties x
+//Addresses
+//Compass degrees
+
+//Q2: Which of the following would be best represented with an enumeration? Choose all that apply
+//
+//Hair colors x
+//T-shirt sizes x
+//Favorite numbers
+//Car speed
+
+//Q3: Which of the following would be best represented with an enumeration? Choose all that apply
+//
+//Car manufacturers x
+//Wi-fi network names
+//Professional basketball teams x
+//Shoe brands
